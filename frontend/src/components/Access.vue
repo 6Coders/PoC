8766f56c68fed6a98cd87aceabe73170ddbacf56
oslaman <oslaman@proton.me>
2024-01-02 17:06:10 +0100

    Implementazione pagina Access

---
+++ frontend/src/components/Access.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app" class="container mt-6">
+        <div class="row">
+            <div class="col-md-6">
+                <h1 class="display-2">Benvenuto</h1>
+                <h2 class="text-body-secondary mb-5">Che tipo di utente sei?</h2>
+                <div>
+                    <button type="button" class="btn btn-primary custom-color btn-lg btn-block d-block mb-5" @click="redirectToManagerPage">Utente Gestore</button>
+                    <button type="button" class="btn btn-primary custom-color btn-lg btn-block d-block" @click="redirectToUserPage">Utente base</button>
+                </div>
+            </div>
+            <div class="col-md-2">
+            </div>
+            <div class="col-md-4">
+                <img src="../assets/6Coders-logo.png" alt="Descrizione dell'immagine" class="img-fluid">
+           </div>
+        </div>
+    </div>
+</template>
+    
+<script>
+    export default {
+      methods: {
+        redirectToManagerPage() {
+          this.$router.push({ name: 'Manager' });
+        },
+        redirectToUserPage() {
+          this.$router.push({ name: 'User' });
+        }
+      }
+    };
+</script>
