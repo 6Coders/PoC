ecef46dce3516204c31807c787fb0b7e03ce196a
unknown <y.lovato01@gmail.com>
2024-01-02 15:58:49 +0100

    Close #1: Creata la struttura del frontend e backend

---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+if __name__ == '__main__':
+    app.run()
