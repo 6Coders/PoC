e647eb19d6cfa81e49a7c9ad508f79d99725c24d
unknown <y.lovato01@gmail.com>
2024-01-05 18:41:02 +0100

    Close #14: aggiunta API GET/files e logica nel frontend , aggiunte le dipendenze per fontawesome su index.html

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
+import os
+import time
+import chatsql
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
-import os
+
 
 UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__)) + '/uploads/'
 ALLOWED_EXTENSIONS = {'sql', 'json'}
@@ -30,6 +33,26 @@ def upload_file():
         file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
         return 'File successfully uploaded'
 
+@app.route('/files', methods=['GET'])
+def get_files():
+    files_dir = app.config['UPLOAD_FOLDER']
+    files = []
+    for filename in os.listdir(files_dir):
+        path = os.path.join(files_dir, filename)
+        size = os.path.getsize(path)
+        mtime = os.path.getmtime(path)
+        date = time.ctime(mtime)
+        name, extension = os.path.splitext(filename)
+        loaded = False #non ancora possibile applicare la logica di caricamento 
+        print(loaded)
+        files.append({
+            'name': name,
+            'extension': extension,
+            'date': date,
+            'size': size,
+            'loaded': loaded
+        })
+    return jsonify(files)
 
 if __name__ == '__main__':
     app.run()
